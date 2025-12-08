45a18c74eacaefe8d0df176435082714d4ee6e91
xdrshjr <476494220@qq.com>
2025-12-08 16:40:56 +0800

    [init] added 0.6b script.

--- skyrl-train/train.sh
+++ skyrl-train/train.sh
@@ -1,4 +1,7 @@
-export WANDB_API_KEY=b85e63cba5515431941adc3e0443a9146bf7d5d5
+export https_proxy=http://127.0.0.1:7897;
+export http_proxy=http://127.0.0.1:7897;
+export all_proxy=socks5://127.0.0.1:7897;
 
+export WANDB_API_KEY=b85e63cba5515431941adc3e0443a9146bf7d5d5
 
 bash examples/mini_swe_agent/run_mini_swe_0.6B.sh
